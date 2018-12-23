c978ea7a5ad9dc07398111f74a624c4e21d54ff6
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-23 19:57:54 +0200

    Change publication title smaller

--- docs/.vuepress/components/Publication.vue
+++ docs/.vuepress/components/Publication.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <span v-html="publication.authors"></span><br>
-    <a v-bind:href="publication.link" class="large"><span v-html="publication.title"></span><OutboundLink /></a>
+    <a v-bind:href="publication.link"><span v-html="publication.title"></span><OutboundLink /></a>
       <span v-if="publication['phd']"><Badge text="Ph.D. Thesis"/></span><br>
     <span v-html="publication.citation"></span>
     <span v-if="publication['doi']">doi:{{publication.doi}}</span>
@@ -15,9 +15,6 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.large
-    font-size 18px
-
 li
     margin-bottom 16px
 </style>
