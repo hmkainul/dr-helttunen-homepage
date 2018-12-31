2c39bab04a6425f269fd7bca90e0e99f9df0b326
Heikki Kainulainen <hmkainul@gmail.com>
2018-12-31 14:15:09 +0200

    Change publication title bold

--- docs/.vuepress/components/Publication.vue
+++ docs/.vuepress/components/Publication.vue
@@ -1,7 +1,7 @@
 <template>
   <li>
     <span v-html="publication.authors"></span><br>
-    <a v-bind:href="publication.link"><span v-html="publication.title"></span><OutboundLink /></a>
+    <a v-bind:href="publication.link"><span v-html="publication.title" class="title"></span><OutboundLink /></a>
       <span v-if="publication['phd']"><Badge text="Ph.D. Thesis"/></span><br>
     <span v-html="publication.citation"></span>
     <span v-if="publication['doi']">doi:{{publication.doi}}</span>
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.title
+    font-weight bold
+
 li
     margin-bottom 16px
 </style>
