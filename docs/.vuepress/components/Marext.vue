de7a9c4182e613cfe5db301af502f08e62991e49
Heikki Kainulainen <hmkainul@gmail.com>
2019-01-16 12:00:42 +0100

    Use Calendar component

--- docs/.vuepress/components/Marext.vue
+++ docs/.vuepress/components/Marext.vue
@@ -3,13 +3,7 @@
   <div class="row1">
     <div class="left1">
       <h2>Macrocyclic Receptors for Selective Anion Extraction</h2>
-      <p>
-        <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-          <path d="M148 288h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12zm108-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 96v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm-96 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm192 0v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm96-260v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h48V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h128V12c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v52h48c26.5 0 48 21.5 48 48zm-48 346V160H48v298c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"
-          />
-        </svg>
-        May 2018 - April 2023
-      </p>
+      <p><Calendar/> May 2018 - April 2023</p>
       <p>The project aims to offer pioneering solutions for the selective binding of biologically and technologically
       important anions. The target anions include anti-inflammatory drugs with carboxylate groups, and valuable
       resources, such as phosphate and precious metals (Au, Pt, Co). The project focuses on advanced synthesis
