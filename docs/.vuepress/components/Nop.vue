a7ed9231845fee9fc708bcd6b1f810a350f7b55c
Heikki Kainulainen <hmkainul@gmail.com>
2019-03-03 12:26:38 +0100

    Add content element

--- docs/.vuepress/components/Nop.vue
+++ docs/.vuepress/components/Nop.vue
@@ -1,3 +1,14 @@
 <template>
-    <Home />
+    <div>
+        <div class="my-content">
+            <Content />
+        </div>
+        <Home />
+    </div>
 </template>
+
+<style lang="stylus">
+.my-content
+    max-width 740px
+    margin auto
+</style>
