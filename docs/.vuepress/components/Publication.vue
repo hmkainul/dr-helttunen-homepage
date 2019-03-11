80c500a28aed639b6971bddbae79dec269a1c7b0
Heikki Kainulainen <hmkainul@gmail.com>
2019-03-11 14:54:34 +0100

    Add thesis link

--- docs/.vuepress/components/Publication.vue
+++ docs/.vuepress/components/Publication.vue
@@ -2,7 +2,7 @@
   <li>
     <span v-html="publication.authors"></span><br>
     <a v-bind:href="publication.link"><span v-html="publication.title" class="title"></span><OutboundLink /></a>
-      <span v-if="publication['phd']"><Badge text="Ph.D. Thesis"/></span><br>
+      <span v-if="publication['phd']"><a href="thesis/"><Badge text="Ph.D. Thesis"/></a></span><br>
     <span v-html="publication.citation"></span>
     <span v-if="publication['doi']">doi:{{publication.doi}}</span>
   </li>
