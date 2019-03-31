74a5be488aa7ed2a439b2180b383399ef66e004d
Heikki Kainulainen <hmkainul@gmail.com>
2019-03-31 20:31:39 +0200

    Add graphical abstract

--- docs/.vuepress/components/Publication.vue
+++ docs/.vuepress/components/Publication.vue
@@ -5,6 +5,8 @@
       <span v-if="publication['phd']"><a href="thesis/"><Badge text="Ph.D. Thesis"/></a></span><br>
     <span v-html="publication.citation"></span>
     <span v-if="publication['doi']">doi:{{publication.doi}}</span>
+    <span v-if="publication['image']"><img v-bind:src="$withBase('/' + publication.image)" /></span>
+    <span v-if="publication['graphicalAbstract']" class="abstract">{{publication.graphicalAbstract}}</span>
   </li>
 </template>
 
@@ -18,6 +20,15 @@ export default {
 .title
     font-weight bold
 
+.abstract
+    font-style italic
+
 li
     margin-bottom 16px
+
+img
+    max-width 100%
+    display block
+    margin-left auto
+    margin-right auto
 </style>
