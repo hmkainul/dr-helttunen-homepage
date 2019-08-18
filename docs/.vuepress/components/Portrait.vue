cf066d6911582ed00a01e248ff6c8c3ae60b816d
Heikki Kainulainen <hmkainul@gmail.com>
2019-08-18 20:22:16 +0300

    Change pic border

--- docs/.vuepress/components/Portrait.vue
+++ docs/.vuepress/components/Portrait.vue
@@ -1,15 +1,15 @@
 <template>
-    <img class="shadow" :src="$withBase('/' + src)" :alt="alt" />
+  <img class="portrait" :src="$withBase('/' + src)" :alt="alt" />
 </template>
 
 <script>
 export default {
-    props: ["src", "alt"]
-}
+  props: ["src", "alt"]
+};
 </script>
 
-<style lang="stylus" scoped>
-.shadow
-    box-shadow 0 4px 8px 0 rgb(0,0,0,0.2)
-    box-shadow 0 6px 20px 0 rgba(0,0,0,0.19)
+<style scoped>
+.portrait {
+  border: 1px solid #eaecef;
+}
 </style>
